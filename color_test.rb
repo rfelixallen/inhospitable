72bffa6d7036753a2af5e0ccca32dff171422967
Robert Allen <rfelixallen@gmail.com>
2016-05-21 12:30:56 -0400

    expanded tests, but failing to use custom colors

--- color_test.rb
+++ color_test.rb
@@ -34,27 +34,60 @@ Ncurses.mvwaddstr(stdscr, 4, 2, "Press any key to continue.")
 grefresh
 erase
 
-#Ncurses.start_color
 Ncurses.mvwaddstr(stdscr, 2, 2, "Testing if this Terminal is capable of using colors.")
 Ncurses.mvwaddstr(stdscr, 3, 2, "Press any key to continue.")
 grefresh
 erase
 
-has_colors = nil
-
 if Ncurses.has_colors?
-	has_colors = true
 	Ncurses.mvwaddstr(stdscr, 2, 2, "Test passed. Terminal can use colors.")
 	Ncurses.mvwaddstr(stdscr, 3, 2, "Press any key to continue.")
 	grefresh
 	erase
 else
-	has_colors = false
 	Ncurses.mvwaddstr(stdscr, 2, 2, "Test failed. Terminal cannot use colors.")
+	Ncurses.mvwaddstr(stdscr, 3, 2, "Press any key to end the test.")
+	grefresh
+	erase
+	Ncurses.endwin
+	exit
+end
+
+Ncurses.start_color
+Ncurses.mvwaddstr(stdscr, 2, 2, "Colors started. You can use #{Ncurses.COLORS} colors and #{Ncurses.COLOR_PAIRS} color pairs.")
+Ncurses.mvwaddstr(stdscr, 3, 2, "Press any key to continue.")
+grefresh
+erase
+
+# init_pair(pair code, forground, background)
+Ncurses.init_pair(1, COLOR_BLACK, COLOR_RED)
+Ncurses.init_pair(2, COLOR_YELLOW, COLOR_BLUE)
+Ncurses.wattron(stdscr,Ncurses.COLOR_PAIR(1))
+Ncurses.mvwaddstr(stdscr, 2, 2, "Color pair initiated. Foreground: Black, Background: Red.")
+Ncurses.wattron(stdscr,Ncurses.COLOR_PAIR(2))
+Ncurses.mvwaddstr(stdscr, 3, 2, "Color pair initiated. Foreground: Yellow, Background: Blue.")
+Ncurses.wattroff(stdscr,Ncurses.COLOR_PAIR(2))
+Ncurses.mvwaddstr(stdscr, 4, 2, "Press any key to continue.")
+grefresh
+erase
+
+if Ncurses.can_change_color?
+	Ncurses.init_color(240,1000,750,750)
+	Ncurses.init_pair(4, 240, COLOR_WHITE)
+	Ncurses.wattron(stdscr,Ncurses.COLOR_PAIR(4))
+	Ncurses.mvwaddstr(stdscr, 2, 2, "Terminal is capable of creating custom RGB colors.")
+	Ncurses.mvwaddstr(stdscr, 3, 2, "Press any key to continue.")
+	grefresh
+	Ncurses.wattroff(stdscr,Ncurses.COLOR_PAIR(4))
+	erase
+else
+	Ncurses.mvwaddstr(stdscr, 2, 2, "Terminal cannot create custom RGB colors.")
 	Ncurses.mvwaddstr(stdscr, 3, 2, "Press any key to continue.")
 	grefresh
 	erase
 end
 
 
+
 Ncurses.endwin
+exit
