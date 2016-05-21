cca1b0d01a709f07d7a106afd473bc528672a53d
Robert Allen <rfelixallen@gmail.com>
2016-05-21 11:49:06 -0400

    created working color test

--- color_test.rb
+++ color_test.rb
@@ -9,6 +9,18 @@ def test
 	end
 end
 
+def grefresh
+Ncurses.refresh
+Ncurses.getch
+end
+
+def erase
+Ncurses.mvwaddstr(stdscr, 2, 2, "                                                                                ")
+Ncurses.mvwaddstr(stdscr, 3, 2, "                                                                                ")
+Ncurses.mvwaddstr(stdscr, 4, 2, "                                                                                ")
+Ncurses.refresh
+end
+
 Ncurses.initscr             # Start Ncurses
 Ncurses.noecho              # Do not show keyboard input at cursor location
 Ncurses.curs_set(0)         # Disable blinking cursor
@@ -19,6 +31,30 @@ Ncurses.keypad(stdscr,true) # Use expanded keyboard characters
 Ncurses.mvwaddstr(stdscr, 2, 2, "This program will test colors in terminal.")
 Ncurses.mvwaddstr(stdscr, 3, 2, "Colors are not yet active.")
 Ncurses.mvwaddstr(stdscr, 4, 2, "Press any key to continue.")
-Ncurses.refresh
-Ncurses.getch
+grefresh
+erase
+
+#Ncurses.start_color
+Ncurses.mvwaddstr(stdscr, 2, 2, "Testing if this Terminal is capable of using colors.")
+Ncurses.mvwaddstr(stdscr, 3, 2, "Press any key to continue.")
+grefresh
+erase
+
+has_colors = nil
+
+if Ncurses.has_colors?
+	has_colors = true
+	Ncurses.mvwaddstr(stdscr, 2, 2, "Test passed. Terminal can use colors.")
+	Ncurses.mvwaddstr(stdscr, 3, 2, "Press any key to continue.")
+	grefresh
+	erase
+else
+	has_colors = false
+	Ncurses.mvwaddstr(stdscr, 2, 2, "Test failed. Terminal cannot use colors.")
+	Ncurses.mvwaddstr(stdscr, 3, 2, "Press any key to continue.")
+	grefresh
+	erase
+end
+
+
 Ncurses.endwin
